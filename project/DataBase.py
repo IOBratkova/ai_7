7599349f016781492fe0fe55c5478e8d5ceae915
Irina Bratkova <bratckova.i@yandex.ru>
2019-12-11 21:32:19 +0300

    Добавлены комменты в файле DataBase

--- project/DataBase.py
+++ project/DataBase.py
@@ -1,19 +1,34 @@
 from Classes import Frame, TextSlot, InheritanceIndex, ListSlot, AtomSlot, FrameSlot
 
+# Список фреймов
 frames = []
 
-# Фрейм Больница: Название, Адрес.
+# Фрейм Больница: адрес, врачи
+# Создаем табличку фрейма и пишем в нее заголовое - Больница
 hospitalFrame = Frame('Больница')
+# Записываем в табличку шаблоны строк без значений. Это адрес и список врачей.
 hospitalFrame.slots.append(TextSlot('адрес', InheritanceIndex.u, None))
 hospitalFrame.slots.append(ListSlot('врачи', InheritanceIndex.u, None))
-frames.append(hospitalFrame) # frames[0]
+frames.append(hospitalFrame)  # frames[0]
+"""
+Получили табличку такую:
+________
+Больница
+________
+Адрес
+Врачи
+________
+и добавили ее в список фреймов
+"""
 
+# Фрейм Доктор: стаж, специальность, прием (дни)
 doctorFrame = Frame('Доктор')
 doctorFrame.slots.append(AtomSlot('стаж', InheritanceIndex.u, None))
 doctorFrame.slots.append(TextSlot('специальность', InheritanceIndex.u, None))
 doctorFrame.slots.append(TextSlot('прием', InheritanceIndex.u, None))
-frames.append(doctorFrame) # frames[1]
+frames.append(doctorFrame)  # frames[1]
 
+# Фрейм Волкова, наследник врача: стаж=40, специальность=кардиолог, прием=пн-пт
 volkovaFrame = Frame('Волкова', frames[1])
 experience = 40
 speciality = 'кардиолог'
@@ -21,8 +36,9 @@ datetime = 'пн-пт'
 volkovaFrame.slots.append(AtomSlot('стаж', InheritanceIndex.u, experience))
 volkovaFrame.slots.append(TextSlot('специальность', InheritanceIndex.u, speciality))
 volkovaFrame.slots.append(TextSlot('прием', InheritanceIndex.u, datetime))
-frames.append(volkovaFrame) # frames[2]
+frames.append(volkovaFrame)  # frames[2]
 
+# Фрейм Новиков, наследник врача: стаж=10, специальность=лор, прием=пср-вс
 novikovFrame = Frame('Новиков', frames[1])
 experience = 10
 speciality = 'лор'
@@ -30,46 +46,65 @@ datetime = 'ср-вс'
 novikovFrame.slots.append(AtomSlot('стаж', InheritanceIndex.u, experience))
 novikovFrame.slots.append(TextSlot('специальность', InheritanceIndex.u, speciality))
 novikovFrame.slots.append(TextSlot('прием', InheritanceIndex.u, datetime))
-frames.append(novikovFrame) # frames[3]
+frames.append(novikovFrame)  # frames[3]
 
+# Фрейм Неболит, наследник больницы: адрес =ул.ОченьДлинная, врачи = [Волкова, Новиков]
+"""
+Все так же, как и больничкой, только теперь мы уточняем:
+_________________________
+Неболит
+_________________________
+Адрес: ул. Очень длинная
+Врачи: Новиков, Волкова
+_________________________
+"""
 nopainFrame = Frame('Неболит', frames[0])
-address = 'ул. Очень длинная, дом Крайне Высокий'
+address = 'ул. Очень длинная'
 doctors = [frames[2], frames[3]]
 nopainFrame.slots.append(TextSlot('адрес', InheritanceIndex.u, address))
 nopainFrame.slots.append(ListSlot('врачи', InheritanceIndex.u, doctors))
-frames.append(nopainFrame) # frames[4]
+frames.append(nopainFrame)  # frames[4]
 
+# Фрейм Талон: время, врач, дата
 talonFrame = Frame('Талон')
+talonFrame.slots.append(TextSlot('дата', InheritanceIndex.u, None))
 talonFrame.slots.append(TextSlot('время', InheritanceIndex.u, None))
 talonFrame.slots.append(FrameSlot('врач', InheritanceIndex.u, None))
-frames.append(talonFrame) # frames[5]
+frames.append(talonFrame)  # frames[5]
 
+# Фрейм Пациент: талон
 patientFrame = Frame('Пациент')
 patientFrame.slots.append(FrameSlot('талон', InheritanceIndex.u, None))
-frames.append(patientFrame) #frame[6]
+frames.append(patientFrame)  # frame[6]
 
+# Фрейм Талон237: дата, время, врач
 talon237Frame = Frame('Талон237', frames[5])
+date = '02.02.2020'
 time = '10:30'
 doctor = frames[2]
+talon237Frame.slots.append(TextSlot('дата', InheritanceIndex.u, date))
 talon237Frame.slots.append(TextSlot('время', InheritanceIndex.u, time))
 talon237Frame.slots.append(FrameSlot('врач', InheritanceIndex.u, doctor))
-frames.append(talon237Frame) #frames[7]
+frames.append(talon237Frame)  # frames[7]
 
+# Фрейм Талон217: дата, время, врач
 talon217Frame = Frame('Талон217', frames[5])
 time = '11:30'
 doctor = frames[3]
+date = '02.02.2020'
+talon217Frame.slots.append(TextSlot('дата', InheritanceIndex.u, date))
 talon217Frame.slots.append(TextSlot('время', InheritanceIndex.u, time))
 talon217Frame.slots.append(FrameSlot('врач', InheritanceIndex.u, doctor))
-frames.append(talon217Frame) #frames[8]
+frames.append(talon217Frame)  # frames[8]
 
+# Фрейм Костя: талон
 kostyaFrame = Frame('Костя', frames[6])
 kostyat = frames[7]
 kostyaFrame.slots.append(FrameSlot('талон', InheritanceIndex.u, kostyat))
-frames.append(kostyaFrame) #frames[9]
+frames.append(kostyaFrame)  # frames[9]
 
+# Фрейм Марина: талон
 marinaFrame = Frame('Марина', frames[6])
 marinat = frames[8]
 marinaFrame.slots.append(FrameSlot('талон', InheritanceIndex.u, marinat))
-frames.append(marinaFrame) #frames[9]
-
-
+frames.append(marinaFrame)  # frames[10]
