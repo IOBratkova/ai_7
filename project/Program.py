f9e9d4a857e6d2f7941c98d4fd5affe23637946f
IOBratkova <bratckova.i@yandex.ru>
2019-12-12 20:41:30 +0300

    Оно не хотело принимать новый файл и я удалила игнор=_=

--- project/Program.py
+++ project/Program.py
@@ -26,35 +26,6 @@ from DataBase import frames
 """
 
 
-def removeTalon(frames_f, talon, newDate, keyWord):
-    s = ''
-    name = ''
-    for frame_n in frames_f:
-        if frame_n.name == talon:
-            for slot in frame_n.slots:
-                if slot.name == keyWord:
-                    slot.text = newDate
-                    s = slot.lisps[0]()
-                if slot.name == 'пациент':
-                    name = slot.frame.name
-                    return name + ', ' + s + ': ' + newDate
-
-
-def getFrame(frames_f, word):
-    for frame_n in frames_f:
-        if frame_n.name == word:
-            return frame_n
-    return None
-
-
-def getAnswerBySlot(frames_f, word1, word3):
-    for frame_n in frames_f:
-        if frame_n.name == word3:
-            for slot in frame_n.slots:
-                if slot.name == word1:
-                    return frame_n
-    return None
-
 
 flag = True
 while flag:
